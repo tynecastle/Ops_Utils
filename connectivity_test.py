3afe4be62402efa2a0e431f0f4ee84966d4e84d6
Andrea Liu <tynecastle@aliyun.com>
2019-01-12 13:00:04 -0500

    add command line options to specify destination IP and port

--- connectivity_test.py
+++ connectivity_test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Test connectivity to particular hosts at a fixed interval and report failed attempts.
+# Test connectivity to particular hosts and report failed attempts.
 #
 # Author : Liu Sibo
 # Email  : liusibojs@dangdang.com
-# Date   : 2019-01-10
+# Date   : 2019-01-12
 
 import socket
 import time
@@ -16,6 +16,8 @@ logfile = '/data/conn_fail_log'
 desthosts = ('192.168.0.1','192.168.0.2','192.168.0.3')
 destport = 80
 interval = 10
+hostname = socket.gethostname()
+localip = socket.gethostbyname(hostname)
 
 def write_to_log(host, port):
     timestamp = time.strftime("%F %T", time.localtime())
@@ -25,10 +27,25 @@ def write_to_log(host, port):
         
 
 def main(args):
-    global interval
+    global desthosts,destport,interval
+    if args.desthost:
+        desthosts = (args.desthost,)
+    if args.destport:
+        destport = args.destport
     if args.interval:
         interval = args.interval
-    while True:
+        while True:
+            for host in desthosts:
+                try:
+                    s = socket.socket()
+                    s.settimeout(3)
+                    s.connect((host, destport))
+                    s.close()
+                except Exception as e:
+                    print(e)
+                    write_to_log(host, destport)
+            time.sleep(interval)
+    else:
         for host in desthosts:
             try:
                 s = socket.socket()
@@ -36,13 +53,13 @@ def main(args):
                 s.connect((host, destport))
                 s.close()
             except Exception as e:
-                print(e)
-                write_to_log(host, destport)
-        time.sleep(interval)
+                print('%s failed to connect to %s on port %s' % (localip, host, destport))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--interval', type=int, help="Time interval between two connecting attempts")
+    parser.add_argument('-d', '--desthost', type=int, help="Destination host IP")
+    parser.add_argument('-p', '--destport', type=int, help="Destination host port")
     args = parser.parse_args()
     main(args)
 
